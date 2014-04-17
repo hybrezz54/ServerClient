66edd7102cb226b437a0cc9bd699ec001038ff87
hybrezz54 <haz540@gmail.com>
2014-04-17 13:26:16 -0400

    Simple Client 4/17/2014

---
+++ Client/src/Client.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.KeyPairGenerator;
+
+
+public class Client implements Runnable {
+	
+	private String hostName;
+	private int port;
+	private BufferedReader in;
+	private PrintWriter out;
+	private Socket socket;
+	private KeyPairGenerator keyGen;
+
+	public Client(String hostName, int port) {
+		this.hostName = hostName;
+		this.port = port;
+		
+		try {
+			socket = new Socket(hostName, port);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(new PrintWriter(socket.getOutputStream()), true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Socket getSocket() {
+		return this.socket;
+	}
+	
+	public void run() {
+		
+	}
+	
+}
